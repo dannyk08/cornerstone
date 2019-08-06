38fc124d683ae7888ae69cc397d1c2917e4eb6db
Daniel Romero <romerodaniel.k90@gmail.com>
2019-08-05 23:50:27 -0700

    react renders an app

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('hello hello')
+import React, { Component } from "react";
+import { render as renderDOM } from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Tada!</h1>
+    )
+  }
+
+}
+
+renderDOM(<App />, document.getElementById('cornerstoneApp'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const { resolve: resolvePath } = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = ({
   mode,
   destFolder
 }) => {
+  const isProduction = mode === 'production'
+  const devtool = isProduction ? 'inline-source-map' : 'source-map'
+
   const config = {
     entry: {
       main: './src/main.js'
     },
     output: {
-      path: resolvePath(__dirname, '..', destFolder),
+      path: resolvePath(__dirname, destFolder),
       filename: "[name].js"
     },
     module: {
@@ -23,7 +28,18 @@ module.exports = ({
         }
       ]
     },
-    mode
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: resolvePath(__dirname, './public/index.html'),
+      })
+    ],
+    externals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM'
+    },
+    mode,
+    devtool
   }
 
   return config
