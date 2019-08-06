a68c137de01b28b103979b3f8902192160a7b430
Daniel Romero <romerodaniel.k90@gmail.com>
2019-08-05 22:58:59 -0700

    set up babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,28 @@ const { resolve: resolvePath } = require('path')
 module.exports = ({
   mode,
   destFolder
-}) => ({
-  entry: {
-    main: './src/main.js'
-  },
-  output: {
-    path: resolvePath(__dirname, '..', destFolder),
-    filename: "[name].js"
-  },
-  mode
-})
+}) => {
+  const config = {
+    entry: {
+      main: './src/main.js'
+    },
+    output: {
+      path: resolvePath(__dirname, '..', destFolder),
+      filename: "[name].js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    },
+    mode
+  }
+
+  return config
+}
