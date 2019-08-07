81c641e171885ac6c80ba2b5c9471b6bd9f9161c
Daniel Romero <romerodaniel.k90@gmail.com>
2019-08-07 01:36:59 -0700

    carousel works

--- src/components/Slider/Carousel.js
+++ src/components/Slider/Carousel.js
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useRef, createRef, useState } from 'react'
 
 import styles from "./Carousel.scss";
 import SliderCard from "./Card";
 import { carouselData } from "./data";
 
+const SLIDE_HEIGHT = 35
+
 const Carousel = () => {
+  const [carouselTopOffset, setCarouselTopOffset] = useState(0)
+
+  function handleCarouselNavigation(multiplier) {
+    setCarouselTopOffset(multiplier * SLIDE_HEIGHT)
+  }
+
   return (
     <section className={styles['Carousel']}>
       <div className={styles['Carousel__controls']}>
-        <span>*</span>
-        <span>*</span>
-        <span>*</span>
+        <span onClick={() => handleCarouselNavigation(0)}>*</span>
+        <span onClick={() => handleCarouselNavigation(1)}>*</span>
+        <span onClick={() => handleCarouselNavigation(2)}>*</span>
       </div>
 
-      {/* {
-        carouselData.map(({ description }, index) => (
-         <SliderCard
-            key={index}
-            description={description}
-            images={images}
-          />
-        ))
-      } */}
-      <div className={styles['Carousel__slides']}>
-        <SliderCard
-          description={carouselData[0].description}
-          images={carouselData[0].images}
-        />
+      <div
+        className={styles['Carousel__slides']}
+      >
+        <div
+          className={styles['Carousel__slides--cards']}
+          style={{
+            top: `calc(-${carouselTopOffset}rem + 1.5rem)`
+          }}
+        >
+          {
+            carouselData.map(({ description, images }, index) => (
+              <SliderCard
+                key={index}
+                description={description}
+                images={images}
+              />
+            ))
+          }
+        </div>
       </div>
     </section>
   )
